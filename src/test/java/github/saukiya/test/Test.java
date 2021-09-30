70d82c8a6eefdab13b304f38325be6530407aafe
Saukiya <saukiya@qq.com>
2021-10-01 00:31:03 +0800

    [test] 规范读取、识别格式
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- src/test/java/github/saukiya/test/Test.java
+++ src/test/java/github/saukiya/test/Test.java
@@ -1,14 +1,12 @@
 package github.saukiya.test;
 
 
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Tuple;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.text.StrLookup;
 import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.text.StrSubstitutor;
 
 import java.io.File;
-import java.text.MessageFormat;
 import java.util.*;
 
 /**
@@ -17,38 +15,59 @@ import java.util.*;
  */
 public class Test {
     public static void main(String[] args) {
-        // 配置读取
-        File file = new File("Z:\\Dev\\Java\\Minecraft\\Project\\SX-Item\\src\\main\\resources\\RandomString\\NewRandom.yml");
+        distinguish();
+        loadData();
+    }
+
+    /**
+     * lore变量识别
+     */
+    public static void distinguish() {
+        String str = "<s:DefaultPrefix>&c炎之洗礼<s:DefaultSuffix> <s:<l:品质>Color><l:品质>";
+
+        StrSubstitutor ss = new StrSubstitutor(new ContextLookup());
+        ss.setVariablePrefix('<');
+        ss.setVariableSuffix('>');
+        ss.setEnableSubstitutionInVariables(true);
+        String resolvedString = ss.replace(str);
+        System.out.println(resolvedString);
+    }
+
+    /**
+     * 加载Random数据
+     */
+    public static void loadData() {
+
+        // 配置读取的方式
+        File file = new File("./src/main/resources/RandomString/NewRandom.yml");
 
         YamlConfiguration yml = YamlConfiguration.loadConfiguration(file);
-        System.out.println(yml.saveToString());
-        Map<String, List<Tuple<Double, String>>> map = new HashMap();
+        Map<String, List<Tuple<Double, String>>> mainMap = new HashMap();
         for (String key : yml.getKeys(false)) {
             Object obj = yml.get(key);
-            System.out.println("----> " + key);
+            System.out.println("----> " + key + " - " + obj.getClass().getSimpleName());
+            //单行 key - v
             if (obj instanceof String) {
-                map.put(key, Collections.singletonList(new Tuple<>(1D,obj.toString())));
-            } else if (obj instanceof List){
+                mainMap.put(key, Collections.singletonList(new Tuple<>(1D,obj.toString())));
+            }
+            // 多行 key - vList
+            else if (obj instanceof List){
                 List<Tuple<Double, String>> list = new ArrayList<>();
                 Object unitObj = ((List<?>) obj).get(0);
                 System.out.println(unitObj.getClass());
                 if (unitObj instanceof Map) {
                     List<Map> listMap = (List<Map>) obj;
-                    for (Map map1 : listMap) {
-                        System.out.println(map1);
+                    for (Map map : listMap) {
+                        System.out.println("?> " + map);
+                        list.add(new Tuple<>(Double.valueOf(map.get("rate").toString()), loadDataString(map.get("string"))));
                     }
-                    continue;
                 }
-                // Deprecated 过时加载方式
-                else {
+                else {// Deprecated
+                    // TODO 标记可转化文本并备份
                     Map<String, Integer> tempMap = new HashMap<>();
                     String value;
                     for (Object objs : (List) obj) {
-                        if (objs instanceof List ) {
-                            value = String.join("/n", (List) objs);
-                        } else {
-                            value = objs.toString();
-                        }
+                        value = loadDataString(objs);
                         tempMap.put(value, tempMap.getOrDefault(value, 0) + 1);
                     }
                     for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
@@ -57,58 +76,35 @@ public class Test {
                 }
 
                 list.sort(Comparator.comparing(Tuple::a));
-                map.put(key, list);
+                mainMap.put(key, list);
             }
         }
         System.out.println("-------------?");
-        for (Map.Entry<String, List<Tuple<Double, String>>> entry : map.entrySet()) {
+        for (Map.Entry<String, List<Tuple<Double, String>>> entry : mainMap.entrySet()) {
             System.out.println("key: " + entry.getKey());
             for (Tuple<Double, String> tuple : entry.getValue()) {
                 System.out.println("- 权重:" + tuple.a() + " -> " + tuple.b());
             }
         }
-//        for (String key : yml.getKeys(false)) {
-//            for (Map.Entry<String, Object> entry : yml.getConfigurationSection(key).getValues(false).entrySet()) {
-//
-//            }
-//            for (String s : ) {
-//                System.out.println();
-//            }
-//        }
-
-//        lore变量识别
-//        String str = "攻击力: <<111>222> <333>";
-//        StrSubstitutor ss = new StrSubstitutor(new ContextLookup());
-//        ss.setVariablePrefix('<');
-//        ss.setVariableSuffix('>');
-//        ss.setEnableSubstitutionInVariables(true);
-//        String resolvedString = ss.replace(str);
-//        System.out.println(resolvedString);
     }
 
-    public List<String> loadRandomData(Object obj) {
+    public static String loadDataString(Object obj) {
+//        if (obj == null) return "";
         if (obj instanceof String) {
-            return Collections.singletonList(obj.toString());
+            return obj.toString().replace("\n", "/n");
         }
-        List<String> list = new ArrayList<>();
         if (obj instanceof List) {
-            for (Object objs : (List) obj) {
-                if (objs instanceof List ) {
-                    list.add(String.join("/n", (Iterable<? extends CharSequence>) objs));
-                } else {
-                    list.add(objs.toString());
-                }
-            }
+            return String.join("/n", (List) obj);
         }
-        return list;
+        return "无法识别";
     }
 
     static class ContextLookup extends StrLookup<String> {
 
         @Override
         public String lookup(String s) {
-            System.out.println(s + " -> 000");
-            return "000";
+            System.out.println(s + " -> [" + s.charAt(0) + "]");
+            return "[" + s.substring(0, 1) + "]";
         }
     }
 
