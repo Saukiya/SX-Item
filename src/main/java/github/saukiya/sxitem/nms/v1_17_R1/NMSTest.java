8b2b78c3db14bf8a794654b99f355f8c3674416f
Saukiya <Saukiya@qq.com>
2021-10-08 16:33:25 +0800

    [test] 看上去是个能用的nms模块
    Signed-off-by: Saukiya <Saukiya@qq.com>

---
+++ src/main/java/github/saukiya/sxitem/nms/v1_17_R1/NMSTest.java
@@ -0,0 +1,9 @@
+package github.saukiya.sxitem.nms.v1_17_R1;
+
+import github.saukiya.sxitem.nms.Test;
+
+public class NMSTest extends Test {
+    public void A() {
+        System.out.println("v1_17_R1");
+    }
+}
