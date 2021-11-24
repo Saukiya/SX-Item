42c77b23beb43a76a710cd24e28d0611eed96972
Saukiya <Saukiya@qq.com>
2021-11-25 04:33:27 +0800

    [test] 同步测试项
    Signed-off-by: Saukiya <Saukiya@qq.com>

---
+++ src/test/java/github/saukiya/sxitem/nms/v1_17_R1/Test.java
@@ -0,0 +1,8 @@
+package github.saukiya.sxitem.nms.v1_17_R1;
+
+public class Test extends github.saukiya.test.nms.base.Test {
+    @Override
+    public void funA() {
+        System.out.println("2333");
+    }
+}
