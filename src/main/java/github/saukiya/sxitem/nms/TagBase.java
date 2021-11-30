a75614c7263a225db13f017211b300cd3d586264
Saukiya <Saukiya@qq.com>
2021-12-01 04:05:59 +0800

    [feat] NBT 实现toJson方法
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- src/main/java/github/saukiya/sxitem/nms/TagBase.java
+++ src/main/java/github/saukiya/sxitem/nms/TagBase.java
@@ -1,13 +1,25 @@
 package github.saukiya.sxitem.nms;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+
 import java.io.DataOutput;
 import java.io.IOException;
 
 public interface TagBase<T> {
+
+    JsonParser JSON_PARSER = new JsonParser();
+
+    Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     void write(DataOutput dataOutput) throws IOException;
 
     T getValue();
 
     TagType getTypeId();
+
+    default String toJson() {
+        return GSON.toJson(JSON_PARSER.parse(String.valueOf(getValue())));
+    }
 }
