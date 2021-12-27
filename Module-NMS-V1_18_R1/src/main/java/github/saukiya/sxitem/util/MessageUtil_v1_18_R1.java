8cc1aace92198ac6df7ab266ef4ce2f80da0f1c7
Saukiya <Saukiya@qq.com>
2021-12-28 05:31:27 +0800

    [pref] MessageUtil 1.重做 BaseComponent 构造器 2.新增1.18版本支持
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- Module-NMS-V1_18_R1/src/main/java/github/saukiya/sxitem/util/MessageUtil_v1_18_R1.java
+++ Module-NMS-V1_18_R1/src/main/java/github/saukiya/sxitem/util/MessageUtil_v1_18_R1.java
@@ -4,10 +4,10 @@ import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.ItemTag;
 import net.md_5.bungee.api.chat.hover.content.Item;
 import net.md_5.bungee.api.chat.hover.content.Text;
-import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_18_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
-public class MessageUtil_v1_17_R1 extends MessageUtil {
+public class MessageUtil_v1_18_R1 extends MessageUtil {
 
     @Override
     public ComponentBuilder componentBuilder() {
@@ -24,7 +24,7 @@ public class MessageUtil_v1_17_R1 extends MessageUtil {
 
         @Override
         public ComponentBuilder show(ItemStack item) {
-            current.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new Item(item.getType().getKey().getKey(), item.getAmount(), ItemTag.ofNbt(String.valueOf(CraftItemStack.asNMSCopy(item).getTag())))));
+            current.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new Item(item.getType().getKey().getKey(), item.getAmount(), ItemTag.ofNbt(String.valueOf(CraftItemStack.asNMSCopy(item).s())))));
             return this;
         }
     }
