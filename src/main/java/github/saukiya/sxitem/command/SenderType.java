7679726470aa0eb3a9d5977ea248ce4e9607c657
Saukiya <Saukiya@qq.com>
2021-12-28 06:41:35 +0800

    [docs]
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- src/main/java/github/saukiya/sxitem/command/SenderType.java
+++ src/main/java/github/saukiya/sxitem/command/SenderType.java
@@ -5,9 +5,16 @@ package github.saukiya.sxitem.command;
  */
 
 public enum SenderType {
+    /**
+     * 玩家
+     */
     PLAYER,
-
+    /**
+     * 控制台
+     */
     CONSOLE,
-
+    /**
+     * 所有
+     */
     ALL
 }
