87a218e73b585aab86f75a34c13597cd7799f66e
Saukiya <Saukiya@qq.com>
2021-12-28 05:31:27 +0800

    [pref] MessageUtil 1.重做 BaseComponent 构造器 2.新增1.18版本支持
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- Module-NMS-V1_13_R2/src/main/java/github/saukiya/sxitem/util/MessageUtil_v1_13_R2.java
+++ Module-NMS-V1_13_R2/src/main/java/github/saukiya/sxitem/util/MessageUtil_v1_13_R2.java
@@ -1,38 +1,31 @@
 package github.saukiya.sxitem.util;
 
-import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.server.v1_13_R2.NBTTagCompound;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public class MessageUtil_v1_13_R2 extends MessageUtil {
 
     @Override
-    public TextComponent getTextComponent(String msg, String showText, String command) {
-        TextComponent tc = new TextComponent(msg);
-        if (showText != null)
-            tc.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7" + showText)}));
-        if (command != null) tc.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
-        return tc;
+    public ComponentBuilder componentBuilder() {
+        return new ComponentBuilderImpl();
     }
 
-    @Override
-    public TranslatableComponent showItem(@Nonnull Material material) {
-        return new TranslatableComponent((material.isBlock() ? "block" : "item") + "." + material.getKey().getNamespace() + "." + material.getKey().getKey());
-    }
+    class ComponentBuilderImpl extends ComponentBuilder {
 
-    @Override
-    public BaseComponent showItem(@Nullable ItemStack item) {
-        if (item == null) item = new ItemStack(Material.AIR);
-        ItemMeta meta = item.getItemMeta();
-        BaseComponent bc = meta != null && meta.hasDisplayName() ? new TextComponent(meta.getDisplayName()) : showItem(item.getType());
-        NBTTagCompound nbt = CraftItemStack.asNMSCopy(item).save(new NBTTagCompound());
-        bc.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new BaseComponent[]{new TextComponent(nbt.toString())}));
-        return bc;
+        @Override
+        public ComponentBuilder show(String text) {
+            current.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7" + text)}));
+            return this;
+        }
+
+        @Override
+        public ComponentBuilder show(ItemStack item) {
+            current.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new BaseComponent[]{new TextComponent(CraftItemStack.asNMSCopy(item).save(new NBTTagCompound()).toString())}));
+            return this;
+        }
     }
 }
