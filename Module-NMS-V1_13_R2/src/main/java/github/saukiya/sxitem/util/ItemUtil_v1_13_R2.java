547b1a2b34c925dccb1c2e5436bdc091355f2933
Saukiya <Saukiya@qq.com>
2022-01-04 08:17:47 +0800

    [fix] Attribute 1.ItemUtil#addAttribute() 修复bug、简化代码、非空判断
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- Module-NMS-V1_13_R2/src/main/java/github/saukiya/sxitem/util/ItemUtil_v1_13_R2.java
+++ Module-NMS-V1_13_R2/src/main/java/github/saukiya/sxitem/util/ItemUtil_v1_13_R2.java
@@ -56,36 +56,42 @@ public class ItemUtil_v1_13_R2 extends ItemUtil {
         ItemMeta meta = item.getItemMeta();
         if (!meta.hasAttributeModifiers()) return null;
         List<AttributeData> list = new ArrayList<>();
-        meta.getAttributeModifiers().forEach((attribute, data) -> list.add(new AttributeData(
-                attribute.name(),
-                data.getUniqueId(),
-                data.getName(),
-                data.getAmount(),
-                data.getOperation().ordinal(),
-                data.getSlot() != null ? data.getSlot().name() : null
-        )));
+        meta.getAttributeModifiers().forEach(
+                (attribute, data) -> list.add(new AttributeData(
+                        attribute.name(),
+                        data.getUniqueId(),
+                        data.getName(),
+                        data.getAmount(),
+                        data.getOperation().ordinal(),
+                        data.getSlot() != null ? data.getSlot().name() : null
+                ))
+        );
         return list;
     }
 
     @Override
     public void addAttributes(ItemStack item, List<AttributeData> list) {
         ItemMeta meta = item.getItemMeta();
-        list.forEach(data -> Arrays.stream(Attribute.values()).filter(attr -> attr.name().equals(data.getAttrName())).findFirst().ifPresent(attribute -> {
-            meta.addAttributeModifier(attribute, new AttributeModifier(data.getUniqueId(), data.getName(), data.getAmount(),
-                    AttributeModifier.Operation.values()[data.getOperation()],
-                    Arrays.stream(EquipmentSlot.values()).filter(s -> s.name().equals(data.getSlot())).findFirst().orElse(null)));
-        }));
+        list.forEach(data -> addAttribute(meta, data));
         item.setItemMeta(meta);
     }
 
     @Override
     public void addAttribute(ItemStack item, AttributeData data) {
-        Arrays.stream(Attribute.values()).filter(attr -> attr.name().equals(data.getName())).findFirst().ifPresent(attribute -> {
-            ItemMeta meta = item.getItemMeta();
-            meta.addAttributeModifier(attribute, new AttributeModifier(data.getUniqueId(), data.getName(), data.getAmount(),
-                    AttributeModifier.Operation.values()[data.getOperation()],
-                    Arrays.stream(EquipmentSlot.values()).filter(s -> s.name().equals(data.getSlot())).findFirst().orElse(null)));
-            item.setItemMeta(meta);
-        });
+        ItemMeta meta = item.getItemMeta();
+        addAttribute(meta, data);
+        item.setItemMeta(meta);
+    }
+
+    public void addAttribute(ItemMeta meta, AttributeData data) {
+        Arrays.stream(Attribute.values()).filter(attr -> attr.name().equals(data.getAttrName())).findFirst().ifPresent(
+                attribute -> meta.addAttributeModifier(attribute, new AttributeModifier(
+                        data.getUniqueId(),
+                        data.getName(),
+                        data.getAmount(),
+                        AttributeModifier.Operation.values()[data.getOperation()],
+                        Arrays.stream(EquipmentSlot.values()).filter(s -> s.name().equals(data.getSlot())).findFirst().orElse(null)
+                ))
+        );
     }
 }
