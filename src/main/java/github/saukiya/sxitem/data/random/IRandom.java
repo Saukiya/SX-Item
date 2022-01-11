9f9e6154a6ad2f6aae7aac0de113c598f3f11bc8
Saukiya <Saukiya@qq.com>
2022-01-12 02:10:33 +0800

    [pref] Random 1.IRandom#register() 添加默认的注册方法
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- src/main/java/github/saukiya/sxitem/data/random/IRandom.java
+++ src/main/java/github/saukiya/sxitem/data/random/IRandom.java
@@ -1,10 +1,25 @@
 package github.saukiya.sxitem.data.random;
 
+import github.saukiya.sxitem.SXItem;
+
 public interface IRandom {
     /**
+     * 替换字符串内容
+     *
      * @param key    处理的key
      * @param docker 缓存
      * @return 返回为空则删除此行
      */
     String replace(String key, RandomDocker docker);
+
+    /**
+     * 注册
+     *
+     * @param types 注册的字符表
+     */
+    default void register(char... types) {
+        for (char type : types) {
+            SXItem.getRandomManager().getRandoms().put(type, this);
+        }
+    }
 }
