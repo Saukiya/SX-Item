1da81868b464e2a5d5d842fe03743dcddb6c0d28
Saukiya <Saukiya@qq.com>
2022-02-22 20:12:47 +0800

    [perf] RandomDocker 1.更改RandomDocker扩展程度 2.更改LockMap和MythicMobs对RandomDocker的支持

--- src/main/java/github/saukiya/sxitem/data/random/randoms/LockStringRandom.java
+++ src/main/java/github/saukiya/sxitem/data/random/randoms/LockStringRandom.java
@@ -8,8 +8,6 @@ public class LockStringRandom implements IRandom {
     @Override
     public String replace(String key, RandomDocker docker) {
         if (docker.getLockMap() == null) return docker.random(key);
-        //LockStringRandom需要直接获取字符串的最终结果，并保存到lockMap中
-        docker.getLockLog().add(key);
         return docker.getLockMap().computeIfAbsent(key, k -> docker.replace(docker.random(key)));
     }
 }
