af8225bfaa91cb9c88a4b94b8d7b39e9c393d55d
Saukiya <Saukiya@qq.com>
2022-12-25 21:28:49 +0800

    [perf] More 1.MythicMobsHelper 支持4.0及5.0 2.NBTItemWrapper 添加建筑者模式 3.PlaceholderHelper 添加日志输出 4.NMS 添加判断class方法
    Signed-off-by: Saukiya <Saukiya@qq.com>

--- src/main/java/github/saukiya/sxitem/nbt/NBTItemWrapper.java
+++ src/main/java/github/saukiya/sxitem/nbt/NBTItemWrapper.java
@@ -1,5 +1,7 @@
 package github.saukiya.sxitem.nbt;
 
+import lombok.AllArgsConstructor;
+
 /**
  * 只对单一ItemStack进行操作的封装类
  * 减少使用CraftItemStack.asNMSCopy
@@ -12,4 +14,28 @@ public interface NBTItemWrapper extends NBTTagWrapper {
      * 保存到当前的ItemStack中
      */
     void save();
+
+    /**
+     * 链式操作器
+     *
+     * @return Builder
+     */
+    default Builder builder() {
+        return new Builder(this);
+    }
+
+    @AllArgsConstructor
+    class Builder {
+
+        NBTItemWrapper handler;
+
+        public Builder set(String key, Object value) {
+            handler.set(key, value);
+            return this;
+        }
+
+        public void save() {
+            handler.save();
+        }
+    }
 }
