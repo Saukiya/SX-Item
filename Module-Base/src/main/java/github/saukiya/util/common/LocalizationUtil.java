cb20cf2ea664db766affe5177ffc97939e66e2c9
Saukiya <Saukiya@qq.com>
2024-09-23 18:38:31 +0800

    本地化优先级(首选语言->系统语言->EN)

--- Module-Base/src/main/java/github/saukiya/util/common/LocalizationUtil.java
+++ Module-Base/src/main/java/github/saukiya/util/common/LocalizationUtil.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.stream.Stream;
@@ -35,43 +37,60 @@ public class LocalizationUtil {
      * @param plugin          当前插件
      */
     public static void saveResource(JavaPlugin plugin) throws IOException, URISyntaxException {
+        saveResource(plugin, "en");
+    }
+
+    /**
+     * <pre>
+     * 保存本地化资源到插件文件夹中
+     * 会将插件内的"Localization/{language}/"中的资源保存到本地
+     * 如果当前语言不支持，则会选择Localization中的首个语言作为替代
+     * 如果文件已存在，则不会进行覆盖。
+     * </pre>
+     *
+     * @param plugin      当前插件
+     * @param defLanguage 默认语言
+     */
+    public static void saveResource(JavaPlugin plugin, String defLanguage) throws IOException, URISyntaxException {
         try (InputStreamReader reader = new InputStreamReader(plugin.getResource("plugin.yml"))) {
             YamlConfiguration pluginYml = YamlConfiguration.loadConfiguration(reader);
-            String language = pluginYml.getString("language", systemLanguage);
+            String curLanguage = pluginYml.getString("language", systemLanguage);
             reader.close();
-            extractResourceFolder(plugin, language);
+            extractResourceFolder(plugin, curLanguage, defLanguage);
         }
     }
 
-    private static void extractResourceFolder(JavaPlugin plugin, String language) throws IOException, URISyntaxException {
+    private static void extractResourceFolder(JavaPlugin plugin, String curLanguage, String defaultLanguage) throws IOException, URISyntaxException {
         File targetFolder = plugin.getDataFolder();
         URL sourceUrl = plugin.getClass().getResource("");
-        try (FileSystem fileSystem = FileSystems.newFileSystem(sourceUrl.toURI(), Collections.emptyMap())) {
-            Path fileSystemPath = fileSystem.getPath("/Localization/");
-            plugin.getLogger().warning("fileSystemPath: " + fileSystemPath);
-            try (val languagePaths = Files.list(fileSystemPath)) {
-                val sourcePath = languagePaths.filter(path -> path.getFileName().toString().equals(language)).findFirst().orElseGet(() -> {
-                    try (val elsePath = Files.list(fileSystemPath)) {
-                        return elsePath.findFirst().get();
-                    } catch (IOException e) {
-                        return null;
-                    }
-                });
-                if (sourcePath ==  null) {
-                    plugin.getLogger().warning("Localization: No Language");
-                    return;
+        try (val fileSystem = FileSystems.newFileSystem(sourceUrl.toURI(), Collections.emptyMap())) {
+            Path localizationPath = fileSystem.getPath("/Localization/");
+            // 判断首选语言是否存在
+            Path languagePath = localizationPath.resolve(curLanguage);
+            if (!Files.exists(languagePath)) {
+                // 判断默认语言是否存在
+                languagePath = localizationPath.resolve(defaultLanguage);
+            }
+            // 两个都不在就随便给一个
+            if (!Files.exists(languagePath)) {
+                try (val localizationStream = Files.list(localizationPath)) {
+                    languagePath = localizationStream.findFirst().orElse(null);
                 }
-                plugin.getLogger().info("Localization: " + sourcePath.getFileName());
-                try (Stream<Path> sourcePaths = Files.walk(sourcePath)) {
-                    for (Path source : sourcePaths.toArray(Path[]::new)) {
-                        Path targetPath = targetFolder.toPath().resolve(sourcePath.relativize(source).toString());
-                        if (Files.exists(targetPath)) continue;
-                        if (Files.isDirectory(source)) {
-                            Files.createDirectory(targetPath);
-                        } else {
-                            plugin.getLogger().info("Copy: " + targetPath);
-                            Files.copy(source, targetPath);
-                        }
+            }
+            if (languagePath == null) {
+                plugin.getLogger().warning("Localization: No Language");
+                return;
+            }
+            plugin.getLogger().info("Localization: " + languagePath.getFileName());
+            try (Stream<Path> sourcePaths = Files.walk(languagePath)) {
+                for (Path source : sourcePaths.toArray(Path[]::new)) {
+                    Path targetPath = targetFolder.toPath().resolve(languagePath.relativize(source).toString());
+                    if (Files.exists(targetPath)) continue;
+                    if (Files.isDirectory(source)) {
+                        Files.createDirectory(targetPath);
+                    } else {
+                        plugin.getLogger().info("Copy: " + targetPath);
+                        Files.copy(source, targetPath);
                     }
                 }
             }
