853c1554eca0ceb095f91189cd07fbf0ee5909ce
Saukiya <Saukiya@qq.com>
2024-09-27 20:20:15 +0800

    ScriptManager 优化
    优化掉ScriptObjectMirror

--- src/main/resources/Localization/zh/Scripts/Global/Global.js
+++ src/main/resources/Localization/zh/Scripts/Global/Global.js
@@ -1,13 +1,22 @@
 /*global SXItem, Bukkit, Arrays, Utils*/
+/** @link https://bukkit.windit.net/javadoc/ **/
+
 globalValue = "globalField" // const 也可以
 /**
  * @class java.util.ArrayList
  */
 ArrayList = Java.type("java.util.ArrayList")
 
-
 function globalFunc(arg) {
     let list = new ArrayList()
     list.add(arg);
     return list.get(0)
+}
+
+/**
+ *
+ * @param [Player]([Player](https://bukkit.windit.net/javadoc/org/bukkit/entity/Player.html))
+ * @type {java(org.bukkit.entity.Player)}
+ */
+function send(player) {
 }
