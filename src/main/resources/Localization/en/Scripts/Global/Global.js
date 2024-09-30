962d4d29c5c8813181d3a019610c3e3c300568d0
Saukiya <Saukiya@qq.com>
2024-09-30 13:03:12 +0800

    JavaScript 默认配置

---
+++ src/main/resources/Localization/en/Scripts/Global/Global.js
@@ -0,0 +1,11 @@
+/*global Bukkit, Arrays, SXItem, listener*/
+/**
+ * Common development documents
+ * Bukkit - https://hub.spigotmc.org/javadocs/bukkit/
+ * Java8 - https://docs.oracle.com/javase/8/docs/api/
+ */
+// You can set your global variables here to use in addition to the global folder in
+/**
+ * @see [ArrayList](https://docs.oracle.com/javase/8/docs/api/)
+ */
+ArrayList = Java.type("java.util.ArrayList")
--- src/main/resources/Localization/zh/Scripts/Global/Global.js
+++ src/main/resources/Localization/zh/Scripts/Global/Global.js
@@ -1,4 +1,11 @@
-/*global Bukkit, Arrays, Utils, SXItem, listener*/
-/** @link https://bukkit.windit.net/javadoc/ **/
-ArrayList = Java.type("java.util.ArrayList")
+/*global Bukkit, Arrays, SXItem, listener*/
+/**
+ * 常用的开发文档
+ * Bukkit - https://bukkit.windit.net/javadoc/
+ * Java8 - https://www.matools.com/api/java8
+ */
 // 可以在此设置你的全局变量 提供给除了Global文件夹的脚本类使用
+/**
+ * @see [ArrayList](https://www.matools.com/doc/java8/java/util/ArrayList.html)
+ */
+ArrayList = Java.type("java.util.ArrayList")
