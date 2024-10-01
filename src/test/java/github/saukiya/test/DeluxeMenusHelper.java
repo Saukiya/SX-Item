246474ea020bca1f0b53e3da2233cd7b6ad0bcc4
Saukiya <Saukiya@qq.com>
2024-10-01 18:09:04 +0800

    test

---
+++ src/test/java/github/saukiya/test/DeluxeMenusHelper.java
@@ -0,0 +1,29 @@
+package github.saukiya.test;
+
+public class DeluxeMenusHelper {
+
+    public static void setup() {
+//        val deluxeMenus = Bukkit.getPluginManager().getPlugin("DeluxeMenus");
+//        if (deluxeMenus == null) {
+//            SXItem.getInst().getLogger().info("DeluxeMenusHelper Disable");
+//            return;
+//        }
+//        Map<String, Object> itemhook =  NMS.privateField(deluxeMenus, "itemHooks");
+//        itemhook.put("sxitem", new SXItemHook());
+//        SXItem.getInst().getLogger().info("DeluxeMenusHelper Enabled");
+    }
+
+//    private static class SXItemHook implements com.extendedclip.deluxemenus.hooks.ItemHook {
+//
+//        @Override
+//        public ItemStack getItem(String... args) {
+//            SXItem.getInst().getLogger().info("Hook: " + Arrays.toString(args));
+//            val itemName = args.length > 0 ? args[0] : null;
+//            val playerName = args.length > 1 ? args[1] : null;
+//            if (args.length > 2) {
+//                args = Arrays.copyOfRange(args, 2, args.length);
+//            }
+//            return SXItem.getItemManager().getItem(itemName, playerName, args);
+//        }
+//    } 
+}
